deb29310d5c834b5c50129fbcb6b23cff70c35df
zinstack625 <klimanov625@outlook.com>
2022-06-23 19:49:40 +0300

    mwsg: initial commit

---
+++ mentor_whirlpool_subject_gathering/__main__.py
@@ -0,0 +1,44 @@
+from asyncio import run, create_task, Queue
+from telebot.async_telebot import AsyncTeleBot
+from os import environ as env, path
+from random import choice
+
+
+running = True
+bot = AsyncTeleBot(env['TELEGRAM_BOT_TOKEN'], parse_mode='html')
+queue = Queue(maxsize=512)
+encouragements = []
+enc_file_path = path.join(path.dirname(__file__), 'encouragements.txt')
+with open(enc_file_path, 'r') as enc_file:
+    for line in enc_file:
+        encouragements.append(line)
+
+
+async def flush_queue():
+    file = open('botlog.txt', 'a')
+    while (running):
+        file.write(await queue.get() + '\n')
+    file.close()
+
+
+@bot.message_handler(commands=['start'])
+async def start(message):
+    await bot.send_message(message.from_user.id, 'Напишите сюда все ваши пожелания по направлениям курсачей\n'
+                                                 'Я буду молчать и слушать очень внимательно')
+
+
+@bot.message_handler(func=lambda _: True)
+async def any_message(message):
+    await queue.put(message.from_user.username + ': ' + message.text)
+    await bot.send_message(message.from_user.id, choice(encouragements))
+
+
+async def main():
+    flusher = create_task(flush_queue())
+    while (running):
+        await bot.polling()
+    print('stopped polling')
+    await flusher
+
+if __name__ == '__main__':
+    run(main())
